f568eb8efb4b1923af747e47638d12d9417efbf9
ImpulsoIMP <youfenix27@gmail.com>
2025-05-20 16:14:56 +0200

    Update marketNFT.tsx

--- src/components/marketNFT.tsx
+++ src/components/marketNFT.tsx
@@ -3,14 +3,29 @@ import { ShoppingCart } from "lucide-react";
 const MarketNFT = () => {
   const items = [
     {
-      name: "NFT Impulso #1",
-      image: "/impdrive.jpg",
-      price: "1.2 SOL",
+      name: "IMP Start",
+      image: "/impstar.png",
+      price: "0.2% IMP",
     },
     {
-      name: "NFT Impulso #2",
+      name: "IMP Drive",
+      image: "/impdrive.jpg",
+      price: "0.4% IMP",
+    },
+     {
+      name: "IMP Core",
       image: "/impcore.png",
-      price: "0.8 SOL",
+      price: "0.6% IMP",
+    },
+     {
+      name: "IMP Quantum",
+      image: "/impquantum.png",
+      price: "0.8% IMP",
+    },
+     {
+      name: "IMP Genesis",
+      image: "/impgenesis.png",
+      price: "1.0% IMP",
     },
   ];
 
@@ -18,7 +33,7 @@ const MarketNFT = () => {
     <div className="min-h-screen bg-gradient-to-r from-[#0a0f2c] to-[#0d153a] text-[#d0d8f6] p-8">
       <header className="text-center mb-12">
         <h1 className="text-4xl font-bold text-[#69a8ff]">Mercado de NFTs</h1>
-      </header>
+</br>  <p>Los primeros NFT del mercado que pagaran dividendos </p> </header>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {items.map((item, index) => (
           <div key={index} className="bg-[#0c1a4b] p-6 rounded-xl shadow-md border border-[#1e2d5c]">
