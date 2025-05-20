6d68d24cee969c8e36a3b2e25bcf1bb324e71881
ImpulsoIMP <youfenix27@gmail.com>
2025-05-20 20:54:01 +0200

    Update marketNFT.tsx

--- src/components/marketNFT.tsx
+++ src/components/marketNFT.tsx
@@ -33,9 +33,14 @@ const MarketNFT = () => {
         </div>
               <h2 className="text-xl font-bold text-[#89b4ff]">{item.name}</h2>
               <p className="my-2">{item.price}</p>
-              <button className="mt-2 inline-flex items-center gap-2 px-4 py-2 bg-[#69a8ff] text-black rounded hover:bg-[#89b4ff] transition">
+              <a
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-2 inline-flex items-center gap-2 px-4 py-2 bg-[#69a8ff] text-black rounded hover:bg-[#89b4ff] transition"
+              >
                 Comprar <ShoppingCart size={18} />
-              </button>
+              </a>
             </div>
           ))}
         </div>
