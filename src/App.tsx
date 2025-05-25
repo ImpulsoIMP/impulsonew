9cb6d1eec90d9d28b46f410766530111f4612fc5
ImpulsoIMP <youfenix27@gmail.com>
2025-05-25 16:58:53 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import MarketNFT from "@/components/marketNFT";
+import Investing from './pages/Investing';
 
 
 const queryClient = new QueryClient();
@@ -19,6 +20,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/marketNFT" element={<MarketNFT />} />
+          <Route path="/investments" element={<Investing />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
